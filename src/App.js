f4fa6560bdfd682c71869ac77b4e48bb19182ed2
eabalderama <eabalderama@gmail.com>
2020-12-03 17:13:41 +0800

    513PM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+const axios = require('axios');
 
 function App() {
+
+  const [meow, setMeow] = useState('');
+  const [quote, setQuote] = useState([]);
+
+  async function getMeow() {
+    try {
+      const response = await axios.get('https://aws.random.cat/meow');
+      setMeow(response.data['file']);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getQuote() {
+    try {
+      const response = await axios.get('https://quote-garden.herokuapp.com/api/v2/quotes/random');
+      setQuote(...quote, {quoteText: response.data.quote['quoteText'], author: response.data.quote['quoteAuthor']});
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getMeow();
+    getQuote();
+  }, []);
+
+  console.log(quote);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <img src={meow} alt=""/>
+     <h6>{quote.author}</h6>
+    <p>{quote.quoteText}</p>
     </div>
   );
 }
